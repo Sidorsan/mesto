5c5cbb2b4de16c9d9d5b59014500bd567f0482ac
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-03-10 17:23:42 +0700

    10.03.22 17.20

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,4 +1,50 @@
-const enableValidation = () => {
-    const formList = document.querySelectorAll('.form');
-    
+const showError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active')
+};
+
+const hideError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+
+    errorElement.textContent = "";
+    errorElement.classList.remove('popup__input-error_active')
+};
+
+const checkValidity = (formElement, inputElement) => {
+    console.log(inputElement.validity);
+    const isInputNotValid = !inputElement.validity.valid;
+
+    if(isInputNotValid) {
+        const errorMessage = inputElement.validationMessage;
+        showError(formElement, inputElement, errorMessage);
+    } else {
+        hideError(formElement, inputElement);
+    };
 }
+
+const setEventListenersValidate = (formElement) => {
+    const inputList = formElement.querySelectorAll('.popup__input');
+
+    inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', (event) => {
+            console.log(event.target.name, event.target.value)
+
+            checkValidity(formElement, inputElement);
+        })
+    })
+}
+
+const enableValidation = () => {
+   const formList = document.querySelectorAll(".popup__form");
+  
+   formList.forEach((formElement) => {
+      formElement.addEventListener("submit", (event) => {
+          event.preventDefault();
+      });
+      setEventListenersValidate(formElement);
+  });
+};
+
+enableValidation();
